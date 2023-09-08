df7b5372d77e0e9913c8efd89f1ec670a8bfff6e
Zachary Lyda <Oth1zlyda1993@gmail.com>
2023-09-07 23:35:32 -0400

    Set up arrays and alert/prompt windows.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,33 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");  //already here to start with
+var characters = [8 - 128];
+var uCase = ["true", "false"];
+var lCase = ["true", "false"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*"]
+
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword() {                                    //already here to start with
+  var password = generatePassword();                          //already here to start with
+  var passwordText = document.querySelector("#password");     //already here to start with
 
-  passwordText.value = password;
+  passwordText.value = password;                              //already here to start with
 
+  window.alert("How many characters?");
+  window.prompt("Do you want to include upper-case letters?");
+  window.prompt("Do you want to include lower-case letters?");
+  window.prompt("Do you want to include special characters?");
+  window.prompt("Do you want to include numbers?");
+
+  if (characters) {
+
+  }
+
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
